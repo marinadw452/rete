65e5043af97c9070c1e4d05b2894012d3e2c82d1
marinadw452 <maaara62626@gmail.com>
2025-09-02 18:58:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,18 @@
 """
 ملف إعدادات نظام طقطق
 """
-import os
-from dotenv import load_dotenv
 
-# تحميل متغيرات البيئة من .env (يشتغل محليًا فقط)
-load_dotenv()
+import os
 
 # ==================== إعدادات تليجرام ====================
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # ==================== إعدادات قاعدة البيانات ====================
- host=os.getenv("PGHOST"),
-    port=os.getenv("PGPORT"),
-    dbname=os.getenv("PGDATABASE"),
-    user=os.getenv("PGUSER"),
-    password=os.getenv("PGPASSWORD")
+PG_HOST = os.getenv("PGHOST")
+PG_PORT = os.getenv("PGPORT")
+PG_DB = os.getenv("PGDATABASE")
+PG_USER = os.getenv("PGUSER")
+PG_PASSWORD = os.getenv("PGPASSWORD")
 
 # ==================== إعدادات النظام ====================
 SUPPORTED_CITIES = ['الرياض', 'جدة']
